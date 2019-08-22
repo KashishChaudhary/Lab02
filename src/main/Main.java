81563248fdadd9ed22b8a0e2e03e6238853fc79c
KashishChaudhary <kashish.chaudhary_cs18@gla.ac.in>
2019-08-22 15:34:24 +0530

    final commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,9 +9,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userName = scanner.nextLine(), trim ();
+        String userName = scanner.nextLine().trim();
         System.out.println("Welcome," + userName + "!");
         scanner.close();
-
     }
 }
