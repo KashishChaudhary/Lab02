85a6345b1ed3ff411e1a467d68c5b21a32b3d727
KashishChaudhary <kashish.chaudhary_cs18@gla.ac.in>
2019-08-24 03:20:14 +0530

    number is checked

--- src/main/Main2.java
+++ src/main/Main2.java
@@ -10,8 +10,11 @@ public class Main2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-
-
+        if (a > 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("negative");
+        }
     }
 }
 
