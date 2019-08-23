fa1af7547be1b3210dfc6f7c409cc867dd618b2d
KashishChaudhary <kashish.chaudhary_cs18@gla.ac.in>
2019-08-24 02:52:40 +0530

    final commit

---
+++ src/main/Kassi.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author Kashish Chaudhary
+ * @version 1.0
+ */
+public class Kassi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine().trim();
+        String userNamee = scanner.nextLine().trim();
+        System.out.println(userName + " University " + userNamee);
+    }
+}
