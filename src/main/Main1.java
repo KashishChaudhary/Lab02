88ad7353b3c942d45e00c6576c188a43803d842a
KashishChaudhary <kashish.chaudhary_cs18@gla.ac.in>
2019-08-24 03:10:36 +0530

    division is done

--- src/main/Main1.java
+++ src/main/Main1.java
@@ -11,12 +11,15 @@ public class Main1 {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int c, d, e;
+        int c, d, e, f;
         c = a + b;
         System.out.println("sum is = " + c);
         d = a - b;
         System.out.println("difference is = " + d);
         e = a * b;
         System.out.println("multiplication is = " + e);
+        f = a / b;
+        System.out.println("division is = " + f);
+
     }
 }
