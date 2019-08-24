438cc88ca366beee4df072df0037a4628ba34681
KashishChaudhary <kashish.chaudhary_cs18@gla.ac.in>
2019-08-24 09:15:28 +0530

    input has been taken

---
+++ src/main/Main3.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author Kashish Chaudhary
+ * @version 1.0
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
+
+    }
+}
